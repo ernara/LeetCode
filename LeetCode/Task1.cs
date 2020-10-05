079d738d3ecce55bd34b328701979d80c03ed58d
Ernestas <ernestas.araminas@ktu.edu>
2020-10-05 18:52:12 +0300

    Add project files.

---
+++ LeetCode/Task1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Task1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            HashSet<int> h = new HashSet<int>(nums);
+
+            for (int i=0;i<nums.Length;i++)
+            {
+                if (h.Contains(target-nums[i]))
+                {
+                    for (int y=i+1;y<nums.Length;y++)
+                    {
+                        if (nums[i]+nums[y]==target)
+                        {
+                            return new int[] { i, y };
+                        }
+                    }
+                }
+            }
+
+            return new int[] { };
+
+        }
+    }
+}
