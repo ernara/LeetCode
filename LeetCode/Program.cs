15dea20deb069646f1b556fb8f560278dfaf73fd
Ernestas <ernestas.araminas@ktu.edu>
2020-10-07 12:14:36 +0300

    Task4 (#4)

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,24 +6,6 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Task3 t = new Task3();
-            //Console.WriteLine(t.LengthOfLongestSubstring("tmmzuxt"));
-            //Console.WriteLine(t.LengthOfLongestSubstring("abcdbefgh"));
-            //Console.WriteLine(t.LengthOfLongestSubstring("aabaab!bb"));
-            //Console.WriteLine(t.LengthOfLongestSubstring2("tmmzuxt"));
-            //Console.WriteLine(t.LengthOfLongestSubstring2("abcdbefgh"));
-            //Console.WriteLine(t.LengthOfLongestSubstring("aabaab!bb"));
-
-            t.LengthOfLongestSubstring("tmmzuxt");
-            Console.WriteLine("---");
-
-            t.LengthOfLongestSubstring("abcdbefgh");
-            Console.WriteLine("---");
-
-            t.LengthOfLongestSubstring("aabaab!bb");
-            Console.WriteLine("---");
-            t.LengthOfLongestSubstring("dvdf");
-
             Task4 tt = new Task4();
 
             Console.WriteLine(tt.FindMedianSortedArrays(new int[] { 1, 2 },new int[] { 3, 4 }));
--- LeetCode/Task4.cs
+++ LeetCode/Task4.cs
@@ -7,15 +7,61 @@ namespace LeetCode
 {
     class Task4
     {
+        /*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+        Follow up: The overall run time complexity should be O(log (m+n)).
+
+        Constraints:
+
+        nums1.length == m
+        nums2.length == n
+        0 <= m <= 1000
+        0 <= n <= 1000
+        1 <= m + n <= 2000
+        -106 <= nums1[i], nums2[i] <= 106*/
+
         public double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
-            //List<int> list = new List<int>();
-            //for (int i=0;i<nums2.Length)
-            //{
+            int i = 0;
+            int y = 0;
+            List<int> list = new List<int>();
 
-            //}
-            //for (int i=0;i)
-            return 1.5;
+            //go threw both arrays and add values to list until we go to one of the index end 
+            while (i<nums1.Length && y<nums2.Length)
+            {
+                if (nums1[i]<nums2[y])
+                {
+                    list.Add(nums1[i]);
+                    i++;
+                }
+                else
+                {
+                    list.Add(nums2[y]);
+                    y++;
+                }
+            }
+
+            //if we still did not finish one of the lists we can do it now
+            while (i < nums1.Length)
+            {
+                list.Add(nums1[i]);
+                i++;
+            }
+
+            while (y < nums2.Length)
+            {
+                list.Add(nums2[y]);
+                y++;
+            }
+
+            if (list.Count == 0)
+            {
+                return 0;
+            }
+            else if (list.Count % 2 == 0)
+            {
+                return (double)(list[list.Count / 2] + list[list.Count / 2 - 1]) / 2;
+            }
+            return list[list.Count/2];
         }
     }
 }
