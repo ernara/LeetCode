5b12b821bd65ae33bf3be19752f67b30910f9f04
Ernestas <ernestas.araminas@ktu.edu>
2021-01-01 20:15:08 +0200

    T12

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,10 +6,8 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Task11 task11 = new Task11();
-            Console.WriteLine(task11.MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
-            Console.WriteLine(task11.MaxArea(new int[] { 4, 3, 2, 1, 4 }));
-            
+            Task12 task12 = new Task12();
+            Console.WriteLine(task12.IntToRoman(666));
         }
     }
 }
---
+++ LeetCode/Task 10-19/Task12.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Task12
+    {
+        public string IntToRoman(int num)
+        {
+            List<int> normalNumbers = new List<int>() {   1,   5,   10, 50,  100, 500, 1000 };
+            List<char> romanNumbers = new List<char>() { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+
+            string s = "";
+
+            for (int i=normalNumbers.Count-1;i>=0;i--)
+            {
+                if(num>=normalNumbers[i])
+                {
+                    if (i % 2 == 0)
+                    {
+                        if(num / normalNumbers[i] >= 4)
+                        {
+                            s += romanNumbers[i];
+                            s += romanNumbers[i + 1];
+                            num += normalNumbers[i];
+                            num -= normalNumbers[i + 1];
+                        }
+                        else
+                        {
+                            while (num >= normalNumbers[i])
+                            {
+                                s += romanNumbers[i];
+                                num -= normalNumbers[i];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (i > 0 && i + 1 < normalNumbers.Count && num >= -normalNumbers[i - 1] + normalNumbers[i + 1])
+                        {
+                            s += romanNumbers[i - 1];
+                            s += romanNumbers[i + 1];
+                            num += normalNumbers[i - 1];
+                            num -= normalNumbers[i + 1];
+                        }
+                        else
+                        {
+                            s += romanNumbers[i];
+                            num -= normalNumbers[i];
+                        }
+                    }
+                }
+            }
+
+            return s;
+        }
+    }
+}
