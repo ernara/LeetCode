918a8d787a06cf9922851779803b46fc16b45007
Ernestas <ernestas.araminas@ktu.edu>
2021-01-05 17:34:44 +0200

    Task18

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,13 +6,9 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Task17 task = new Task17();
-            var v = task.LetterCombinations("");
+            Task18 task18 = new Task18();
+            task18.FourSum(new int[] { 1, -2, -5, -4, -3, 3, 3, 5 }, -11);
 
-            foreach (var item in v)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
---
+++ LeetCode/Task 10-19/Task18.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class Task18
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numbers2 = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numbers2.ContainsKey(nums[i]))
+                {
+                    numbers2[nums[i]]++;
+                }
+                else
+                {
+                    numbers2.Add(nums[i], 1);
+                }
+            }
+
+            List<int> numbers = new List<int>();
+
+            foreach (var v in numbers2)
+            {
+                int count = Math.Min(4, v.Value);
+
+                for (int i = 0; i < count; i++)
+                {
+                    numbers.Add(v.Key);
+                }
+            }
+
+            numbers.Sort();
+
+            HashSet<string> checkResult = new HashSet<string>();
+
+            int difference;
+
+            List<int> list;
+
+            for (int i = 0; i < numbers.Count - 3; i++)
+            {
+                for (int y = i + 1; y < numbers.Count - 2; y++)
+                {
+                    for (int z = y + 1; z < numbers.Count-1; z++)
+                    {
+                        for (int q = z + 1; q < numbers.Count; q++)
+                        {
+                            difference = numbers[i] + numbers[y] + numbers[z] + numbers[q];
+
+                            if (difference==target)
+                            {
+                                list = new List<int>() { numbers[i], numbers[y], numbers[z], numbers[q] };
+                                list.Sort();
+                                checkResult.Add(String.Join(",",list));
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            int[][] result = new int[checkResult.Count][];
+
+            int ii = 0;
+
+            foreach (var v in checkResult)
+            {
+                result[ii] = new int[4];
+                result[ii++] = v.Split(',').Select(Int32.Parse).ToArray();
+            }
+
+            return result;
+        }
+    }
+}
