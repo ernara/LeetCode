081d466f9f0dd56084647c2a0e9ae970178d06e5
Ernestas <ernestas.araminas@ktu.edu>
2021-01-21 20:52:33 +0200

    Task35

---
+++ LeetCode/Task 20-29/Task22.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_20_29
+{
+    class Task22
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+        }
+
+        public void Function(int max, int left = 0;int right=0, string s="")
+        {
+            if (s)
+        }
+    }
+}
---
+++ LeetCode/Tasl 30-39/Task35.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Task35
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i=0;i<nums.Length;i++)
+            {
+                if (nums[i]>=target)
+                {
+                    return i;
+                }
+            }
+            return nums.Length;
+        }
+    }
+}
