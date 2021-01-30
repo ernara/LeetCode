ace716d2ae4449054248765cc0c9c8722d8a2654
Ernestas <ernestas.araminas@ktu.edu>
2021-01-30 20:45:28 +0200

    Task 41 and 43

---
+++ LeetCode/Task 40-49/Task41.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    class Task41
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!d.ContainsKey(nums[i]))
+                {
+                    d.Add(nums[i], nums[i]);
+                }
+
+            }
+
+            bool b = true;
+            int ii = 1;
+
+            while (b)
+            {
+                if (!d.ContainsKey(ii))
+                {
+                    return ii;
+                }
+                ii++;
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/Task 40-49/Task43.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace LeetCode
+{
+    class Task43
+    {
+        public string Multiply(string num1, string num2)
+        {
+            return (BigInteger.Parse(num1)* BigInteger.Parse(num2)).ToString();
+        }
+    }
+}
