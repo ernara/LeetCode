e45140145290b4af0134ae501a52b6043853be5b
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-08 00:02:06 +0300

    task55

---
+++ LeetCode/Task 50-59/Task55.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    internal class Task55
+    {
+        public bool CanJump(int[] nums)
+        {
+            int jumps = nums[0]-1;
+            for (int i=1;i<nums.Length; i++)
+            {
+                if (jumps == -1)
+                {
+                    return false;
+                }
+
+                if (nums[i] > jumps)
+                {
+                    jumps = nums[i];
+                }
+
+                jumps--;
+            }
+
+            return true;
+        }
+    }
+}
