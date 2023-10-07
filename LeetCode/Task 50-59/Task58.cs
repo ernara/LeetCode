7d6db9e286b89b7ba2b8ad160505bfd382c780d9
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-07 21:22:58 +0300

    Task58

---
+++ LeetCode/Task 50-59/Task58.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_50_59
+{
+    internal class Task58
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (s.Contains(' '))
+            {
+                int l = 1;
+                for (int i = s.Length - 1; i > 0; i--)
+                    if (s[i] != ' ')
+                    {
+                        i--;
+                        for (; i >= 0; i--)
+                        {
+                            if (s[i] == ' ')
+                            {
+                                return l;
+                            }
+                            l++;
+                        }
+                    }
+                return l;
+            }
+
+            return s.Length;
+            
+        }
+    }
+}
