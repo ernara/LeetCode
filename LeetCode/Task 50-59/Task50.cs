9957be88bf652dd864ea17d763d8d8a607c0259b
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-08 19:31:36 +0300

    Task50

---
+++ LeetCode/Task 50-59/Task50.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_50_59
+{
+    internal class Task50
+    {
+        public double MyPow(double x, int n)
+        {
+            if (n == 0 || x == 1) return 1;
+            if (n < 0)
+            {
+                x = 1 / x;
+                n *= -n;
+            }
+            return CalculatePow(x, n);
+        }
+
+        private double CalculatePow(double x, long n)
+        {
+            if (n == 1) return x;
+            double halfPow = CalculatePow(x, n / 2);
+
+            return (n % 2 == 0) ? halfPow * halfPow : x * halfPow * halfPow;
+        }
+    }
+}
