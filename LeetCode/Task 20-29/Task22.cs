5e43e81d7ff17b95a3fd946356cd4a6918677058
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-10 17:12:40 +0300

    Task22

---
+++ LeetCode/Task 20-29/Task22.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_20_29
+{
+    internal class Task22
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            List<string> result = new List<string>();
+            GenerateParenthesis(n, 0, 0, "", result);
+            return result; 
+        }
+        public void GenerateParenthesis(int n,int left, int right, string s, IList<string> result)
+        {
+            if (n == left && left==right)
+            {
+                result.Add(s);
+            }
+            else
+            {
+                if (left < n)
+                {
+                    GenerateParenthesis(n, left + 1, right, s + "(", result);
+                }
+                if (right < left)
+                {
+                    GenerateParenthesis(n, left, right+1, s + ")", result);
+                }
+            }
+        }
+    }
+}
