bf8b85bfe56f9a0fa6a51efa2bab7101a4649d75
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-17 15:44:47 +0300

    Task23 faster than 99% but lazy

---
+++ LeetCode/Task 20-29/Task23.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_20_29
+{
+    internal class Task23
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            List<int> values = new List<int>();
+
+            for (int i = 0; i < lists.Length; i++)
+            {
+                while (lists[i] != null)
+                {
+                    values.Add(lists[i].val);
+                    lists[i] = lists[i].next;
+                }
+            }
+
+            Console.WriteLine(values.Count);
+            if (values.Count == 0) { return new ListNode().next; }
+
+            values.Sort();
+            values.Reverse();
+
+            ListNode result = new(values[0]);
+
+            for (int i = 1; i < values.Count; i++)
+            {
+                result = new ListNode(values[i], result);
+            }
+
+            return result;
+
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
