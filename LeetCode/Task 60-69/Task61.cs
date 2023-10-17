6f1d6c68e84523ea7bb636343af35521f9d4aaff
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-17 10:56:05 +0300

    Task61 LazyWay

---
+++ LeetCode/Task 60-69/Task61.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    internal class Task61
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            List<int> values = new List<int>();
+
+            while (head != null)
+            {
+                values.Add(head.val);
+                head = head.next;
+            }
+
+            if (values.Count == 0)
+            {
+                return head;
+            }
+
+            values.Reverse();
+
+
+            k %= values.Count;
+
+            ListNode result = new ListNode(values[k]);
+
+            for (int i = k+1; i < values.Count; i++)
+            {
+                result = new ListNode(values[i], result);
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                result = new ListNode(values[i], result);
+            }
+
+            return result;
+
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
