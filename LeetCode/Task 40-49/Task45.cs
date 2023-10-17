eeeb41a48c826c8261dc11a1ba9ac405739f8aff
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-17 03:08:15 +0300

    Task45. Pretty fun. Need to use Dictionary or otherwise to slow

---
+++ LeetCode/Task 40-49/Task45.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_40_49
+{
+    internal class Task45
+    {
+        public int Jump(int[] nums)
+        {
+            if (nums.Length == 1) return 0;
+
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            Jump(nums, 0, 0, d);
+            return d[nums.Length - 1];
+        }
+
+        public void Jump(int[] nums, int i, int jumps, Dictionary<int, int> d)
+        {
+            if (i>=nums.Length-1)
+            {
+                return;
+            }
+            else if (nums[i]==0)
+            {
+                return;
+            }
+            else
+            {
+                for (int y = nums[i];y>0;y--)
+                {
+                    if (!d.ContainsKey(i+y))
+                    {
+                        d.Add(i + y, jumps + 1);
+                        Jump(nums, i + y, jumps + 1, d);
+                    }
+                    else if (d[i+y] > jumps+1)
+                    {
+                        d[i + y] = jumps + 1;
+                        Jump(nums, i + y, jumps + 1, d);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
