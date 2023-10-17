78c61b54794b9298fca25e92a079354b481e86f4
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-17 16:22:00 +0300

    Task24 Seems smart

---
+++ LeetCode/Task 20-29/Task24.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Text;
+
+namespace LeetCode.Task_20_29
+{
+    internal class Task24
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            ListNode first = head;
+            ListNode second = head;
+
+            while (first!=null && first.next!=null)
+            {
+                second = first.next;
+                (first.val,second.val) = (second.val,first.val);
+                first = second.next;
+            }
+
+            return head;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
