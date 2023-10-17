c4cf7e3db795ac22b405e0259e79cee71d668bf7
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-17 16:56:58 +0300

    Task29

---
+++ LeetCode/Task 20-29/Task29.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_20_29
+{
+    internal class Task29
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if (dividend == int.MaxValue && divisor == 1)
+                return int.MaxValue;
+            else if (dividend == int.MaxValue && divisor == -1)
+                return int.MinValue + 1;
+            else if ((dividend == int.MinValue && divisor == -1))
+                return int.MaxValue;
+            else if (((dividend == int.MinValue && divisor == 1)))
+                return int.MinValue;
+            else return dividend / divisor;
+        }
+    }
+}
