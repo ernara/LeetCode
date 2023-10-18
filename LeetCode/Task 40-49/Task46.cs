63c35a9bf6f126132c51e1bb182a05370f8b7d8e
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-19 02:15:57 +0300

    Task46

--- LeetCode/Task 40-49/Task46.cs
+++ LeetCode/Task 40-49/Task46.cs
@@ -1,21 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 
 namespace LeetCode.Task_40_49
 {
     internal class Task46
     {
-        public IList<IList<int>> Permute(int[] nums)
+        public List<IList<int>> Permute(int[] nums)
         {
-            return new List<IList<int>>();
+            List<IList<int>> result = new List<IList<int>>();
+            int k = nums.Length;
+            Permute(k,nums.ToList(),new List<int>(), result);
+            return result;
         }
-
-        public void Permute2 ()
+        public void Permute(int length,List<int> nums, List<int> list, List<IList<int>> result)
         {
-            
-        }
+            if (list.Count == length)
+            {
+                result.Add(list.ToList());
+                return;
+            }
+            else if(nums.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    int o = nums[i];
+                    list.Add(o);
+                    nums.RemoveAt(i);
+                    Permute(length,new List<int>(nums), new List<int>(list), result);
+                    list.RemoveAt(list.Count-1);
+                    nums.Insert(0,o);
+                }
+            }
 
+            return;
+        }
     }
 }
