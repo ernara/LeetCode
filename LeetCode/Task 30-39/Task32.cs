f8e87a7a2f0f93446a5ec821f84e3b9dee21d9d7
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-18 23:11:30 +0300

    Task32

---
+++ LeetCode/Task 30-39/Task32.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_30_39
+{
+    internal class Task32
+    {
+        public int LongestValidParentheses(string s)
+        {
+
+            int max = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int current = 0;
+                int open = 0;
+                int closed = 0;
+                for (int y=i;y<s.Length;y++)
+                {
+                    if (s[y] =='(')
+                    {
+                        open++;
+                    }
+                    else if (open>closed)
+                    {
+                        closed++;
+                        current++;
+                        if (current * 2 > max && open == closed)
+                        {
+                            max = current * 2;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                
+            }
+            return max;
+        }
+    }
+}
