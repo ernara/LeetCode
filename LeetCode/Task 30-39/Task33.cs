5bec714b4139ae313a1e8338b31379300b227402
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-18 23:20:21 +0300

    Task33

---
+++ LeetCode/Task 30-39/Task33.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_30_39
+{
+    internal class Task33
+    {
+        public int Search(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
