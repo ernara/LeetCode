d81b24b70550dd39617adb5bb41ca2af52472969
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-19 21:39:21 +0300

    Task75 could use bubble algrithm

---
+++ LeetCode/Task 70-79/Task75.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Task_70_79
+{
+    internal class Task75
+    {
+        public void SortColors(int[] nums)
+        {
+            Array.Sort(nums);
+        }
+    }
+}
