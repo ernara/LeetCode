810fb8d694c652aa3dfcbf203862adb160777585
Ernestas Araminas <51124312+ernara@users.noreply.github.com>
2023-10-19 21:13:35 +0300

    Task53 Took to long

--- LeetCode/Task 50-59/Task53.cs
+++ LeetCode/Task 50-59/Task53.cs
@@ -1,14 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace LeetCode.Task_50_59
 {
     internal class Task53
     {
-        //public int MaxSubArray(int[] nums)
-        //{
+        public int MaxSubArray(int[] nums)
+        {
+            int result = int.MinValue;
+            int best = int.MinValue;
+            int index = 0;
 
-        //}
+            for (int i = 0;i<nums.Length;i++)
+            {
+                if (nums[i]>=0)
+                {
+                    result = nums[i];
+                    best = nums[i];
+                    index = i+1;
+                    break;
+                }
+                if (nums[i]>best)
+                {
+                    best= nums[i];
+                }
+            }
+
+            if (best<0) { return best; }
+            
+
+            for (int i = index; i<nums.Length; i++)
+            {
+                result += nums[i];
+                
+                if (result > best)
+                {
+                    best = result;
+                }
+
+                if (result < 0)
+                {
+                    result = 0;
+                }
+            }
+
+            return best;
+        }
     }
 }
